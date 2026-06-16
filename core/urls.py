e18fa877279b4cc91cd6497713d5e52dd6f78517
prajjwaldixit96-dev <prajjwaldixit96@gmail.com>
2026-06-16 13:05:43 +0530

    fix media serving in production

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from portfolio import views
 
 urlpatterns = [
@@ -10,6 +11,10 @@ urlpatterns = [
     path('contact/submit/', views.contact_submit, name='contact_submit'),
 ]
 
-# Media files serve karo development mein
+# Media files serve karo - development aur production dono mein
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
